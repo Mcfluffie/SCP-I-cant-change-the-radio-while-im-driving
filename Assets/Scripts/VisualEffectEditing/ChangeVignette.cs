c8b263523b8e4ad70b11c0b7375cb58dee2ff420
nick <10249960@student.sae.edu.au>
2023-03-07 16:35:55 +0800

    To be tested changes for shaders

--- Assets/Scripts/VisualEffectEditing/ChangeVignette.cs
+++ Assets/Scripts/VisualEffectEditing/ChangeVignette.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
-using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR;
 
 public class ChangeVignette : MonoBehaviour
 {
@@ -17,36 +17,52 @@ public class ChangeVignette : MonoBehaviour
     public float saturation;
 
 
+    private InputDevice targetDevice;
+
+    private void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
+        foreach (var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+        }
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+        }
+
+        
+    }
 
     // Update is called once per frame
     void Update()
     {
-        //if (xr controller button b is used, use below code)
-        //{
-            //add x amount to the strength of each variable in this script to allow maddness increase
-        //}
+        targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
+        if (primaryButtonValue)
+            Debug.Log("pressing the button");
 
-        // the volume profile of the post processing is name is profile (feels a bit redundant but needs to stay for the project prototype)
-        VolumeProfile profile = globalVolume.sharedProfile;
+        //VolumeProfile profile = globalVolume.sharedProfile;
 
 
-        UnityEngine.Rendering.VolumeProfile volumeProfile = GetComponent<UnityEngine.Rendering.Volume>()?.profile;
-        if (!volumeProfile) throw new System.NullReferenceException(nameof(UnityEngine.Rendering.VolumeProfile));
+        //UnityEngine.Rendering.VolumeProfile volumeProfile = GetComponent<UnityEngine.Rendering.Volume>()?.profile;
+        //if (!volumeProfile) throw new System.NullReferenceException(nameof(UnityEngine.Rendering.VolumeProfile));
 
-        UnityEngine.Rendering.Universal.Vignette vignette;
+        //UnityEngine.Rendering.Universal.Vignette vignette;
 
-        if (!volumeProfile.TryGet(out vignette)) throw new System.NullReferenceException(nameof(vignette));
+        //if (!volumeProfile.TryGet(out vignette)) throw new System.NullReferenceException(nameof(vignette));
 
-        vignette.intensity.Override(strength);
+        //vignette.intensity.Override(strength);
 
-        // get the specific part of the unity engine rendering to access the colour adjustments override in the post processing volume
-        UnityEngine.Rendering.Universal.ColorAdjustments colourAdj;
+        //// get the specific part of the unity engine rendering to access the colour adjustments override in the post processing volume
+        //UnityEngine.Rendering.Universal.ColorAdjustments colourAdj;
 
-        // if volume profile is null try and find one via the unity engine code stated above
-        // then attempt to create an exeption (throw exepetion) so it can find the correct variable required since URP doesn't like post processing
-        if (!volumeProfile.TryGet(out colourAdj)) throw new System.NullReferenceException(nameof(colourAdj));
+        //// if volume profile is null try and find one via the unity engine code stated above
+        //// then attempt to create an exeption (throw exepetion) so it can find the correct variable required since URP doesn't like post processing
+        //if (!volumeProfile.TryGet(out colourAdj)) throw new System.NullReferenceException(nameof(colourAdj));
 
-        colourAdj.saturation.Override(saturation);
+        //colourAdj.saturation.Override(saturation);
 
 
 
