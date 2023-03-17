b14350da4b0e6b04644bb5979ba9c7743bca934b
avatarmech2020 <86036829+avatarmech2020@users.noreply.github.com>
2023-03-17 16:22:46 +0800

    Vignette properly lerps and has cases points the sanity bar will add entities

--- Assets/Scripts/VisualEffectEditing/ChangeVignette.cs
+++ Assets/Scripts/VisualEffectEditing/ChangeVignette.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.XR;
@@ -14,24 +15,29 @@ public class ChangeVignette : MonoBehaviour
 
     [Range (-100f, 100f)]
     public float saturation;
+    private float currentStrength;
+    private float strengthToReach;
 
-    public float strengthIncrease;
-
+    public float timer;
     public float lerpTime;
+
 
     private InputDevice targetDevice;
 
+    [Range (0, 100f)]
+    public float sanityBar;
+
+
     private void Start()
     {
-        
 
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
 
 
         // create a list of input devices and find the one with specific characteristics 
@@ -49,9 +55,11 @@ public class ChangeVignette : MonoBehaviour
         // find amungst the flaming ruins the controller set in the variables'primary button if it has it and give the bool of that primary button
         targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
 
-        if (primaryButtonValue)
+        if (primaryButtonValue || Input.GetKeyDown(KeyCode.K))
         {
-            ChangeVignetteStrength();
+            InsanityIncrease();
+
+            
         }
 
 //#if UNITY_EDITOR
@@ -86,7 +94,7 @@ public class ChangeVignette : MonoBehaviour
 
         if (!volumeProfile.TryGet(out vignette)) throw new System.NullReferenceException(nameof(vignette));
 
-        vignette.intensity.Override(strength);
+        vignette.intensity.Override(strength / 1.5f);
 
         // get the specific part of the unity engine rendering to access the colour adjustments override in the post processing volume
         UnityEngine.Rendering.Universal.ColorAdjustments colourAdj;
@@ -97,21 +105,91 @@ public class ChangeVignette : MonoBehaviour
 
         colourAdj.saturation.Override(saturation);
 
-
-
+       
+        
     }
+
+    public void InsanityIncrease()
+    {
+        // below will be based off of the scripts running to make 
+        sanityBar += 15f;
+        SanityMeter();
+        strengthToReach = currentStrength + 1f;
+        ChangeVignetteStrength();
+        
+    }
+
+    public void SanityMeter()
+    {
+        // this code affects what events can occur
+
+        // reset timer for sanity removal
+
+        if (sanityBar >= 80)
+        {
+            // activate scripted events to have a chance to happen at this point
+            Debug.Log("top brachet");
+
+        }
+        else if (sanityBar >= 65)
+        {
+            Debug.Log("65");
+        }
+        else if (sanityBar >= 60)
+        {
+            Debug.Log("60");
+        }
+        else if (sanityBar >= 55)
+        {
+            Debug.Log("55");
+        }
+        else if (sanityBar >= 30)
+        {
+            Debug.Log("30");
+        }
+      
+
+
+        if (sanityBar >= 100)
+        {
+            sanityBar = 100;
+        }
+    }
+
+    // Sanity and ChangeVignetteStrength need to be affected at once when the change comes in
 
     public void ChangeVignetteStrength()
     {
         Debug.Log("pressing the button");
 
-        Shader.SetGlobalFloat(Shader.PropertyToID("_Bloodiness"), strength);
+        currentStrength = strength;
 
+        // starts the coroutine and give it the timer it will take in seconds
+        StartCoroutine(LerpingVignette(2));
 
+        
+
+        //strengthToReach += strengthIncrease
 
         //strength += 0.1f;
-        
-        strength = Mathf.Lerp(strength, strength + strengthIncrease, lerpTime * Time.deltaTime);
+
+    }
+
+    IEnumerator LerpingVignette(float time)
+    {
+        float i = 0;
+        float rate = 1 / time;
+
+        while(i <= 1f)
+        {
+            Debug.Log(i);
+            Debug.Log(currentStrength);
+            strength = Mathf.Lerp(currentStrength, currentStrength + 0.2f, i);
+            Shader.SetGlobalFloat(Shader.PropertyToID("_Bloodiness"), strength);
+            i += Time.deltaTime * rate;
+            yield return 0;
+
+        }
     }
 
     private void OnDisable()
