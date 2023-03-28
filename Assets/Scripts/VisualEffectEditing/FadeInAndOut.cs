b33dc19ae96e096b7afe9e42a7189cdd15e9914a
avatarmech2020 <1022986@student.sae.edu.au>
2023-03-28 10:28:09 +0800

    fade code done and ready for implementation

--- Assets/Scripts/VisualEffectEditing/FadeInAndOut.cs
+++ Assets/Scripts/VisualEffectEditing/FadeInAndOut.cs
@@ -4,27 +4,51 @@ using UnityEngine;
 
 public class FadeInAndOut : MonoBehaviour
 {
-
+    
     public float fadeDuration;
     public Color fadeColor;
+    public Material fadeMaterial;
     public Renderer rend;
 
 
     // Start is called before the first frame update
     private void Awake()
     {
-        
+        FadeIn();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            FadeOut();
+        }
+
+        if (Input.GetKeyUp(KeyCode.K))
+        {
+            FadeIn();
+        }
+
+    }
+
+
+
+    public void FadeOut()
+    {
+        Fade(0, 1);
         
     }
 
-    public void FadeOut(float alphaIn, float alphaOut)
+    public void FadeIn()
     {
+        Fade(1, 0);
+    }
 
+
+    public void Fade(float alphaIn, float alphaOut)
+    {
+        
         StartCoroutine(Fading(alphaIn, alphaOut));
 
     }
@@ -34,11 +58,14 @@ public class FadeInAndOut : MonoBehaviour
         float timer = 0;
         while(timer <= fadeDuration)
         {
-
-            Color newColour = fadeColor;
+            Debug.Log("its fading");
+            Color newColour = fadeMaterial.color;
             newColour.a = Mathf.Lerp(alphaIn, alphaOut, timer / fadeDuration);
 
-            rend.material.SetColor("_Color", newColour);
+            Debug.Log(newColour.a);
+            fadeMaterial.color = newColour;
+            // couldn't find the proper ID string for the material at all
+            // rend.material.SetColor("_Color", newColour); 
 
             timer += Time.deltaTime;
             yield return null;
